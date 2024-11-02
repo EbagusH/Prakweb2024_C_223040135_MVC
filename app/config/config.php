7d2abb8ec6788c872f5c2824477936f384231045
Eggy Bagus <eggybagushidayatullah@gmail.com>
2024-11-02 20:57:24 +0700

    PHP MVC

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/Prakweb_2024_C_223040135/Perorangan/phpmvc/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
---
+++ app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class User_model
+{
+    private $nama = 'Eggy';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
